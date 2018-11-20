96abfc4b61379d5957147914f7cd3b3bf0926764
raniaSayedEye <rania.sayed@eye-ltd-eg.com>
2018-11-20 19:28:51 +0200

    setup socket on client

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(4200,()=>console.log('listening....'));
+var server = app.listen(8000,()=>console.log('listening....'));
 
 //static files
 app.use(express.static('public'));
@@ -11,6 +11,6 @@ app.use(express.static('public'));
 //socket setup
 var io = socket(server);
 
-io.on('connection',()=>{
-    console.log('start socket connection');
+io.on('connection',(socket)=>{
+    console.log('start socket connection',socket.id);
 })
