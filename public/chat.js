10115d611723ea76d70359f6e137d7be570fb16e
raniaSayedEye <rania.sayed@eye-ltd-eg.com>
2018-11-20 20:13:45 +0200

    add socket broadcast

--- public/chat.js
+++ public/chat.js
@@ -7,8 +7,11 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 //emit events
+
+//handle sending messages
 btn.addEventListener('click',() =>{
     socket.emit('chat',{
         message:message.value,
@@ -16,7 +19,18 @@ btn.addEventListener('click',() =>{
     });
 });
 
+//handle message typing
+message.addEventListener('keypress',()=>{
+    socket.emit('typing',handle.value);
+})
+
 //listen for incomming messages
 socket.on('chat',(data)=>{
+    feedback.innerHTML ="";
     output.innerHTML += "<p><strong >"+ data.handle +"</strong>: "+data.message +"</p>";
+});
+
+//listen for message typing event
+socket.on('typing',(data)=>{
+    feedback.innerHTML = "<p><em>"+ data +" is typing a message</em>";
 });
